a5975af7117a77155eb07f9c242e32df33b3fce0
Tarcisio <tarcisio.cwb@hotmail.com>
2021-12-15 20:51:59 +0100

    Implementing the navigation Bar

---
+++ Full_Blog_Sample/components/layout/main-navigation.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+function MainNavigation() {
+  return (
+    <header>
+      <nav>
+        <ul>
+        <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default MainNavigation;
