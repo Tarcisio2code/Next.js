802d315dc1b17969755d7ee0ec91dd79bef49ff9
Tarcisio <tarcisio.cwb@hotmail.com>
2021-12-20 22:48:30 +0100

    Components for outputting a detail from a post

---
+++ Full_Blog_Sample/components/posts/post-detail/post-content.js
@@ -0,0 +1,22 @@
+import classes from "./post-content.module.css";
+import PostHeader from "./post-header";
+
+const DUMMY_POST = {
+  slug: "post-1",
+  title: "Getting Started with NextJS",
+  image: "getting-started-nextjs.png",
+  date: "2022-02-10",
+  content: "# This is a first post",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
+export default PostContent;
