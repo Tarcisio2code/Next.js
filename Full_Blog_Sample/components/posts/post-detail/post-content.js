800a088819f1be8033234b639cadd603ee4ccd43
Tarcisio <tarcisio.cwb@hotmail.com>
2021-12-21 21:46:24 +0100

    add React Markdown component

--- Full_Blog_Sample/components/posts/post-detail/post-content.js
+++ Full_Blog_Sample/components/posts/post-detail/post-content.js
@@ -1,5 +1,6 @@
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
+import ReactMarkdown from "react-markdown";
 
 const DUMMY_POST = {
   slug: "post-1",
@@ -15,7 +16,7 @@ function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 }
