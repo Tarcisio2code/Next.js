eb75a6e49499ec3dff6978b058ffd8694c49516c
Tarcisio <tarcisio.cwb@hotmail.com>
2021-12-27 23:12:02 +0100

    Fetch data from markdown files

--- Full_Blog_Sample/pages/posts/index.js
+++ Full_Blog_Sample/pages/posts/index.js
@@ -1,41 +1,17 @@
 import AllPosts from "../../components/posts/all-posts";
+import { getAllPosts } from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-    {
-      slug: "post-1",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR (server-side rendering).",
-      date: "2022-02-10",
-    },
-    {
-      slug: "post-2",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR (server-side rendering).",
-      date: "2022-02-10",
-    },
-    {
-      slug: "post-3",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR (server-side rendering).",
-      date: "2022-02-10",
-    },
-    {
-      slug: "post-4",
-      title: "Getting Started with NextJS",
-      image: "getting-started-nextjs.png",
-      excerpt:
-        "NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in SSR (server-side rendering).",
-      date: "2022-02-10",
-    },
-  ];
-  
-function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS}/>
+function AllPostsPage(props) {
+  return <AllPosts posts={props.posts}/>
 }
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
+
 export default AllPostsPage;
