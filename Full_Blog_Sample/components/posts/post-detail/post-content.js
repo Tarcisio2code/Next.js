59650110e707c97e1ed357749a446c6e0a995e53
Tarcisio <tarcisio.cwb@hotmail.com>
2022-01-10 20:16:14 +0100

    Using paragraphs to better render a linked image

--- Full_Blog_Sample/components/posts/post-detail/post-content.js
+++ Full_Blog_Sample/components/posts/post-detail/post-content.js
@@ -1,17 +1,46 @@
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
-function PostContent(props) { 
-  const { post } = props; 
-
- 
+function PostContent(props) {
+  const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }
