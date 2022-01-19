422d8cb536496f6869e66d987260dd6663804c06
Tarcisio <tarcisio.cwb@hotmail.com>
2022-01-19 23:23:21 +0100

    Use mongoDB package to store data in a database

--- Full_Blog_Sample/pages/api/contact.js
+++ Full_Blog_Sample/pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { ClientSession, Collection, MongoClient } from "mongodb";
+
+async function handler(req, res) {
   // only executes on the server-side. the recives and request only executes on the server-side.
   if (req.method === "POST") {
     const { email, name, message } = req.body;
@@ -14,13 +16,33 @@ function handler(req, res) {
       return;
     }
 
-    // Store it in a database
     const newMessage = {
       email,
       name,
       message,
     };
-    console.log(newMessage);
+
+    let client;
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://tarcisio:course2021@cluster0.cldyn.mongodb.net/my-site?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
 
     res
       .status(201)
