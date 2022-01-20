8aa991bc1a685adac489f21584e9c53c0759fc4c
Tarcisio <tarcisio.cwb@hotmail.com>
2022-01-20 22:48:52 +0100

    Adding UI Feedback With Notifications

--- Full_Blog_Sample/components/contact/contact-form.js
+++ Full_Blog_Sample/components/contact/contact-form.js
@@ -1,26 +1,86 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
+
+async function sendContactData(contactDetails) {
+  const response = await fetch("api/contact", {
+    method: "POST",
+    body: JSON.stringify(contactDetails),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+}
 
 function ContactForm() {
   const [entredEmail, setEntredMail] = useState("");
   const [entredName, setEntredName] = useState("");
   const [entredMessage, setEntredMessage] = useState("");
+  const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
+  const [requestError, setRequestError] = useState();
 
-  function sendMessageHandler(event) {
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus == "error"){
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  },[requestStatus]);
+
+  async function sendMessageHandler(event) {
     event.preventDefault();
 
-    fetch("api/contact", {
-      method: "POST",
-      body: JSON.stringify({
+    setRequestStatus("pending");
+    try {
+      await sendContactData({
         email: entredEmail,
         name: entredName,
         message: entredMessage,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+      });
+      setRequestStatus("success")
+      setEntredMessage("");
+      setEntredMail("");
+      setEntredName("");
+    } catch (error) {
+      setRequestError(error.message)
+      setRequestStatus("error")
+    }
   }
+
+  let notification;
+
+  if (requestStatus === "pending" ){
+    notification = {
+      status: "pending",
+      title: "Sending menssage...",
+      message: "Your message is on its way!",
+    };
+  }
+
+  if (requestStatus === "success" ){
+    notification = {
+      status: "pending",
+      title: "Success!",
+      message: "Message sent successfully!",
+    };
+  }
+
+  if (requestStatus === "error" ){
+    notification = {
+      status: "error",
+      title: "Error!",
+      message: requestError,
+    };
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
@@ -61,6 +121,13 @@ function ContactForm() {
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification 
+          status={notification.status} 
+          title={notification.title} 
+          message={notification.message} 
+        />
+      )}
     </section>
   );
 }
