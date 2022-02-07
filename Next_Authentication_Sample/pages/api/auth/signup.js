0642bcf431efc53bf521d9639e6601e8bde179c2
Tarcisio <tarcisio.cwb@hotmail.com>
2022-02-07 21:12:33 +0100

    Adding A User Signup API Route

---
+++ Next_Authentication_Sample/pages/api/auth/signup.js
@@ -0,0 +1,34 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  const data = req.body;
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    !password.trim().length < 7
+  ) {
+    return res.status(422).json({
+      message:
+        "Invalid input - password should also be at least 7 characters long.",
+    });
+  }
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "Created user!" });
+}
+
+export default handler;
