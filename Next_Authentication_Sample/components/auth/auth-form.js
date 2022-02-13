40e287bd66c216f1aa55446907c2622fdf4e6e29
Tarcisio <tarcisio.cwb@hotmail.com>
2022-02-13 18:33:19 +0100

    Added component to create a new user

--- Next_Authentication_Sample/components/auth/auth-form.js
+++ Next_Authentication_Sample/components/auth/auth-form.js
@@ -1,33 +1,76 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useRef, useState } from "react";
+import classes from "./auth-form.module.css";
+
+async function createUser(email, password) {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+
+  return data;
+}
 
 function AuthForm() {
+  const emailImputRef = useRef();
+  const passwordImputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  async function submitHandler(event) {
+    event.preventDefault();
+
+    const entredEmail = emailImputRef.current.value;
+    const entredPassword = passwordImputRef.current.value;
+
+    if (isLogin) {
+      // log user in
+    } else {
+      try {
+        const result = await createUser(entredEmail, entredPassword);
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit = {submitHandler} >
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailImputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordImputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
