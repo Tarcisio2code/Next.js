a3705b6cafabde2b5761a7fd1e30f6f2055eb20e
Tarcisio2code <tarcisio.cwb@gmail.com>
2022-03-01 22:26:30 +0100

    Protecting the "Auth" Page

--- Next_Authentication_Sample/components/auth/auth-form.js
+++ Next_Authentication_Sample/components/auth/auth-form.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 import classes from "./auth-form.module.css";
 import { signIn } from "next-auth/client";
+import { useRouter } from "next/router";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -24,6 +25,7 @@ function AuthForm() {
   const passwordImputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -44,6 +46,7 @@ function AuthForm() {
 
       if (!result.error) {
         // set some auth state
+        router.replace("/profile");
       }
     } else {
       try {
--- Next_Authentication_Sample/pages/auth.js
+++ Next_Authentication_Sample/pages/auth.js
@@ -1,6 +1,25 @@
-import AuthForm from '../components/auth/auth-form';
+import { getSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import AuthForm from "../components/auth/auth-form";
 
 function AuthPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace("/");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return <AuthForm />;
 }
 
