831a32ccb0161414758bd4917d85a65bf41208c1
Tarcisio2code <tarcisio.cwb@gmail.com>
2022-03-03 22:51:29 +0100

    Adding the "Change Password" Logic

--- Next_Authentication_Sample/pages/api/user/change-password.js
+++ Next_Authentication_Sample/pages/api/user/change-password.js
@@ -1,16 +1,54 @@
 import { getSession } from "next-auth/client";
+import { hashPassword, verifyPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
 
 function handler(req, res) {
   if (req.method !== "PATCH") {
     return;
   }
- 
+
   const session = getSession({ req: req });
 
   if (!session) {
     req.status(401).json({ message: "Not authenticated" });
     return;
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await connectToDatabase();
+
+  const usersCollection = client.db().collection("users");
+
+  const user = await usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found." });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEqual) {
+    res.status(403).json({ message: "Invalid password." });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  usersCollection.updateOne(
+    { email: userEmail }, 
+    { $set: { newPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: "Password updated!" });
 }
 
 export default handler;
